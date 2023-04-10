143bf622cb71c9f52a079abe124b2cfaa67f55e5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-10 15:38:21 +0200

    bump coverage

--- colormapping_test.go
+++ colormapping_test.go
@@ -12,6 +12,8 @@ func TestNewMapping(t *testing.T) {
 	err := m.LoadDefaults()
 	assert.NoError(t, err)
 
+	assert.NotNil(t, m.GetColors())
+
 	c := m.GetColor("scifi_nodes:blacktile2", 0)
 	assert.NotNil(t, c)
 	assert.Equal(t, uint8(20), c.R)
@@ -24,4 +26,29 @@ func TestNewMapping(t *testing.T) {
 	c = m.GetColor("unifiedbricks:brickblock_multicolor_dark", 100)
 	assert.NotNil(t, c)
 
+}
+
+func TestNewMappingErrors(t *testing.T) {
+	m := colormapping.NewColorMapping()
+	count, err := m.LoadBytes([]byte("stuff"))
+	assert.Error(t, err)
+	assert.Equal(t, 0, count)
+
+	count, err = m.LoadBytes([]byte("my:node invalid_r 0 0"))
+	assert.Error(t, err)
+	assert.Equal(t, 0, count)
+
+	count, err = m.LoadBytes([]byte("my:node 0 invalid_g 0"))
+	assert.Error(t, err)
+	assert.Equal(t, 0, count)
+
+	count, err = m.LoadBytes([]byte("my:node 0 0 invalid_b"))
+	assert.Error(t, err)
+	assert.Equal(t, 0, count)
+}
+
+func TestLoadErrors(t *testing.T) {
+	m := colormapping.NewColorMapping()
+	_, err := m.LoadVFSColors("bogus.txt")
+	assert.Error(t, err)
 }
